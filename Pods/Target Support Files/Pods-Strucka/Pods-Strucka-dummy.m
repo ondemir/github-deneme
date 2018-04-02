de8348e8b62edd3b583c54299322a1b402bd3e1f
Onur Demir <ondemir@Onurs-MacBook-Pro.local>
2018-04-02 11:06:15 +0300

    first commit

---
+++ Pods/Target Support Files/Pods-Strucka/Pods-Strucka-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Strucka : NSObject
+@end
+@implementation PodsDummy_Pods_Strucka
+@end
---
+++ Strucka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Strucka
+//
+//  Created by Onur Demir on 2.04.2018.
+//  Copyright © 2018 Onur Demir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
